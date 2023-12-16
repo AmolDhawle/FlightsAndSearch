bae9f5b871c9cc32060f6b4a5a1397a78214a8e0
Amol Dhawle <amoldhawle2@gmail.com>
2023-12-16 13:33:07 +0530

    Added flight-create request  middleware

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,24 @@
+const validateCreateFlight = (req, res, next) => {
+    if(
+        !req.body.flightNumber  ||
+        !req.body.airplaneId  ||
+        !req.body.departureAirportId  ||
+        !req.body.arrivalAirportId  ||
+        !req.body.arrivalTime ||
+        !req.body.departuretime  ||
+        !req.body.price
+    ){
+        return res.status(400).json({
+            data: {},
+            status: false,
+            message: 'Invalid request body for create flight',
+            err: 'Missing mandatory property to create a flight'
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validateCreateFlight
+}
